ceb08d1d7a96a85e30a30a2cfc0fe1d4ac1c962a
Jesus Ramirez <jesus.ramirez@orbis.com.pe>
2019-11-19 16:14:43 -0500

    create mvc project and api

--- app/source/webmvc/Program.cs
+++ app/source/webmvc/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace webapi
+namespace webmvc
 {
     public class Program
     {
